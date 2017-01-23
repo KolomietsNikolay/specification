c5063124aab35420f9cf0d8865764a3a7d39c272
kolomiecNikolay <kolomiec558@gmail.com>
2017-01-23 18:52:01 +0200

    Update specification

---
+++ SpecificationWEB/Specification/LessedBalanceUserSpecification.cs
@@ -0,0 +1,16 @@
+﻿using SpecificationWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpecificationWEB.Specification
+{
+    public class LessedBalanceUserSpecification : ExpessionEntity<User>
+    {
+        public LessedBalanceUserSpecification(int balanceLess)
+        {
+            this.expression = x => x.Balance < balanceLess;
+        }
+    }
+}
