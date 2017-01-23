0dc6f5020064a4717deea4e15b5c6d26acf5b73b
kolomiecNikolay <kolomiec558@gmail.com>
2017-01-23 18:23:31 +0200

    Fixed bug

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using SpecificationWEB.Context;
+using SpecificationWEB.Models;
+using SpecificationWEB.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace SpecificationWEB.Repository
+{
+    public class RepositoryBase
+    {
+        private SysytemUsersDbContext context;
+
+        public RepositoryBase()
+        {
+            this.context = new SysytemUsersDbContext();
+        }
+
+        public IEnumerable<User> GetAll(ISpecification<User> specification)
+        {
+            return this.context.Users.Where(specification.IsSatisfiedBy().Compile());
+        }
+    }
+}
